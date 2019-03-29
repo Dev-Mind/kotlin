d3bdb7aa6cbdabc9130ec383a9f8c890f1be33e8
Guillaume EHRET <guillaume@dev-mind.fr>
2019-03-29 10:48:19 +0100

    Kotlin and its functionalities

---
+++ src/main/java/com/devmind/kotlin/Test.java
@@ -0,0 +1,17 @@
+package com.devmind.kotlin;
+
+/**
+ * @author devmind
+ */
+public class Test {
+
+    /**
+     * Kotlin est complétement interopérable avec Java
+     * @param args
+     */
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker("Guillaume", "EHRET", 42, "USA");
+
+        System.out.println("Hello Devoxx " + speaker);
+    }
+}
